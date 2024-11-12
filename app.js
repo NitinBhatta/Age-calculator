20ca1fca4e0bf5e63fcb93760ddde7618397f297
nitin.bhatta0225@gmail.com <nitin.bhatta0225@gmail.com>
2024-11-13 09:18:54 +1100

    "Added app.js with age calculation logic and index.html with age calculator form"

---
+++ app.js
@@ -0,0 +1,14 @@
+let currentyear;
+let birthyear;
+
+currentyear = 2024;
+birthyear = 2008;
+
+let userage = currentyear - birthyear;
+console.log(userage);
+
+if (userage > 18) {
+  console.log("eligible");
+} else {
+  console.log("not eligible");
+}
